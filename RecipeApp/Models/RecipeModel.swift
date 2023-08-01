3a07d8dc3e211bf6fdd626aed352a15d88b26116
Elaidzha1940 <eli080293@gmail.com>
2023-08-02 02:43:48 +0300

    RecipeApp + added RecipeModel

--- RecipeApp/Models/RecipeModel.swift
+++ RecipeApp/Models/RecipeModel.swift
@@ -10,3 +10,29 @@
 //  */
 
 import Foundation
+
+enum Category: String {
+    
+     case breakfast = "Breakfast"
+     case soup = "Soup"
+     case salad = "Salad"
+     case appetizer = "Appetizer"
+     case main = "Main"
+     case side = "Side"
+     case dessert = "Dessert"
+     case snack = "Snack"
+     case drink = "Drink"
+}
+
+struct Recipe: Identifiable {
+    
+    let id = UUID()
+    let name: String
+    let image: String
+    let description: String
+    let ingredients: String
+    let directions: String
+    let category: Category
+    let datePublished: String
+    let url: String
+}
