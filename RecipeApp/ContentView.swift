6d577bd176add148792c9cdf0fa41778b0b54702
Elaidzha1940 <eli080293@gmail.com>
2023-08-01 23:47:09 +0300

    RecipeApp

---
+++ RecipeApp/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  RecipeApp
+//
+//  Created by Elaidzha Shchukin on 01.08.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        VStack {
+            
+            Image(systemName: "globe")
+               
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RecipeApp/RecipeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeAppApp.swift
+//  RecipeApp
+//
+//  Created by Elaidzha Shchukin on 01.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct RecipeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
