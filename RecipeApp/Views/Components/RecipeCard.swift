46b5d953e5e683ed99415083cf4a2675b7134dd8
Elaidzha1940 <eli080293@gmail.com>
2023-08-02 21:06:50 +0300

    RecipeCard

--- RecipeApp/Views/Components/RecipeCard.swift
+++ RecipeApp/Views/Components/RecipeCard.swift
@@ -22,11 +22,17 @@ struct RecipeCard: View {
                 image
             } placeholder: {
                 Image(systemName: "photo")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 40, height: 40, alignment: .center)
+                    .foregroundColor(.white.opacity(0.7))
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
             }
         }
         .frame(width: 160, height: 220, alignment: .top)
         .background(LinearGradient(gradient: Gradient(colors: [Color(.gray).opacity(0.3), Color(.gray)]), startPoint: .top, endPoint: .bottom))
         .clipShape(RoundedRectangle(cornerRadius: 20, style: .continuous))
+        .shadow(color: Color.black.opacity(0.3), radius: 15, x: 0, y: 10)
     }
 }
 
