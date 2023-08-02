165f29a0fce1dbb66649086f9b93c451f33ffc6b
Elaidzha1940 <eli080293@gmail.com>
2023-08-02 21:42:52 +0300

    RecipeView

--- RecipeApp/Views/Detail/RecipeView.swift
+++ RecipeApp/Views/Detail/RecipeView.swift
@@ -12,13 +12,44 @@
 import SwiftUI
 
 struct RecipeView: View {
+    
+    var recipe: Recipe
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        ScrollView {
+            
+            AsyncImage(url: URL(string: recipe.image)) { image in
+                image
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                   
+            } placeholder: {
+                Image(systemName: "photo")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 100, height: 100, alignment: .center)
+                    .foregroundColor(.white.opacity(0.7))
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                
+            }
+            .frame(height: 300)
+            .background(LinearGradient(gradient: Gradient(colors: [Color(.gray).opacity(0.3), Color(.gray)]), startPoint: .top, endPoint: .bottom))
+            
+            VStack(spacing: 30) {
+                
+                Text(recipe.name)
+                    .font(.largeTitle)
+                    .bold()
+                    .multilineTextAlignment(.center)
+            }
+        }
+        .ignoresSafeArea(.container, edges: .top)
     }
 }
 
 struct RecipeView_Previews: PreviewProvider {
     static var previews: some View {
-        RecipeView()
+        RecipeView(recipe: Recipe.all[0])
     }
 }
