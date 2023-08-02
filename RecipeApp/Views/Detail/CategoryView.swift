7e8bd3697d6cd7a6b7be832ca186eed6b501a0a8
Elaidzha1940 <eli080293@gmail.com>
2023-08-02 23:50:42 +0300

    added CategoryView

---
+++ RecipeApp/Views/Detail/CategoryView.swift
@@ -0,0 +1,24 @@
+//  /*
+//
+//  Project: RecipeApp
+//  File: CategoryView.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 02.08.2023
+//
+//  Status: in progress | Decorated
+//
+//  */
+
+import SwiftUI
+
+struct CategoryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CategoryView_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryView()
+    }
+}
