10b4fcaece314d69dd2ea0f2d088f627fbd879c7
Elaidzha1940 <eli080293@gmail.com>
2023-08-02 21:29:15 +0300

    RecipeList

--- RecipeApp/Views/Components/RecipeList.swift
+++ RecipeApp/Views/Components/RecipeList.swift
@@ -12,13 +12,34 @@
 import SwiftUI
 
 struct RecipeList: View {
+    
+    var recipes: [Recipe]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        VStack{
+            HStack {
+                Text("\(recipes.count) \(recipes.count > 1 ? "recipes" : "recipe")")
+                    .font(.headline)
+                    .fontWeight(.medium)
+                    .opacity(0.7)
+                
+                Spacer()
+            }
+            
+            LazyVGrid(columns: [GridItem(.adaptive(minimum: 160), spacing: 15)], spacing: 15) {
+                ForEach(recipes) { recipe in
+                    RecipeCard(recipe: recipe)
+                }
+            }
+            .padding(.top)
+        }
+        .padding(.horizontal)
     }
 }
 
 struct RecipeList_Previews: PreviewProvider {
     static var previews: some View {
-        RecipeList()
+        RecipeList(recipes: Recipe.all)
     }
 }
