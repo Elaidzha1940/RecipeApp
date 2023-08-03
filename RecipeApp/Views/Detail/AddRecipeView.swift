0fca4a301235c09c636ec43834acf95801662e2d
Elaidzha1940 <eli080293@gmail.com>
2023-08-03 20:31:10 +0300

    added AddRecipeView

---
+++ RecipeApp/Views/Detail/AddRecipeView.swift
@@ -0,0 +1,24 @@
+//  /*
+//
+//  Project: RecipeApp
+//  File: AddRecipeView.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 03.08.2023
+//
+//  Status: in progress | Decorated
+//
+//  */
+
+import SwiftUI
+
+struct AddRecipeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AddRecipeView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddRecipeView()
+    }
+}
