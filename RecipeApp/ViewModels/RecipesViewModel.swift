8bacf709c30db48d285f6fac4faef4f27f974639
Elaidzha1940 <eli080293@gmail.com>
2023-08-04 08:42:50 +0300

    added RecipesViewModel

---
+++ RecipeApp/ViewModels/RecipesViewModel.swift
@@ -0,0 +1,24 @@
+//  /*
+//
+//  Project: RecipeApp
+//  File: RecipesViewModel.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 04.08.2023
+//
+//  Status: in progress | Decorated
+//
+//  /*
+
+import SwiftUI
+
+struct RecipesViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RecipesViewModel_Previews: PreviewProvider {
+    static var previews: some View {
+        RecipesViewModel()
+    }
+}
